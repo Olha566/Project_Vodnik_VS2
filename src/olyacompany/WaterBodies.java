02cd22b0f7ed8589d249c8507a88ba9284bfcf6a
olha.nosal <olgetta1221@gmail.com>
2023-07-19 13:37:18 +0300

    start new profect

---
+++ src/olyacompany/WaterBodies.java
@@ -0,0 +1,13 @@
+package olyacompany;
+
+public interface WaterBodies {
+    String waterName();
+
+    String type();
+
+    double area();
+
+    String generalInfo();
+
+    void showRoute();
+}
---
+++ src/olyacompany/impl/Ocean.java
@@ -0,0 +1,25 @@
+package olyacompany.impl;
+
+public class Ocean {
+    public Ocean() {
+    }
+
+    public String waterName() {
+        return null;
+    }
+
+    public String type() {
+        return null;
+    }
+
+    public double area() {
+        return 0.0;
+    }
+
+    public String generalInfo() {
+        return null;
+    }
+
+    public void showRoute() {
+    }
+}
